409668172602db23cd5b2f2f651d60e352c9ba73
Amine <mohammed.sghir@cgi.com>
2017-05-03 10:21:38 +0200

    display summoner detail

--- app/src/main/java/com/aminesghir/leaguehelper/SummonerDetailActivity.java
+++ app/src/main/java/com/aminesghir/leaguehelper/SummonerDetailActivity.java
@@ -1,9 +1,12 @@
 package com.aminesghir.leaguehelper;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 public class SummonerDetailActivity extends AppCompatActivity {
@@ -15,12 +18,35 @@ public class SummonerDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summoner_detail);
 
-        getSummonerNameFromIntent();
+        DisplayDataTask displayData = new DisplayDataTask();
+        displayData.execute();
 
-        ((TextView)findViewById(R.id.title)).setText(summonerName);
+    }
 
-        ((TextView)findViewById(R.id.idTextView)).setText(DataProvider.getSummonerByName(summonerName));
+    private class DisplayDataTask extends AsyncTask<Void, Void, String>{
 
+        @Override
+        protected void onPreExecute() {
+            getSummonerNameFromIntent();
+            ((TextView)findViewById(R.id.title)).setText(summonerName);
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            return DataProvider.getSummonerByName(summonerName);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try {
+                JSONObject json = new JSONObject(s);
+                ((TextView)findViewById(R.id.idTextView)).setText(json.getJSONObject(summonerName.toLowerCase()).getString("id"));
+                ((TextView)findViewById(R.id.nameTextView)).setText(json.getJSONObject(summonerName.toLowerCase()).getString("name"));
+                ((TextView)findViewById(R.id.levelTextView)).setText(json.getJSONObject(summonerName.toLowerCase()).getString("summonerLevel"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void getSummonerNameFromIntent() {
